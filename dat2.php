dcb1c3e5e640e2592a1b6cff6eaaffa06ce14e7d
qtdu <116353460+qtdu@users.noreply.github.com>
2024-07-21 12:07:37 +0700

    Create dat2.php

---
+++ dat2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Data Page</title>
+</head>
+<body>
+    <h1>Data from PHP</h1>
+    <p id="data">This is the data from the PHP page.</p>
+</body>
+</html>
